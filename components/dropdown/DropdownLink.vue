e2505614bae66710d34f7b776dc4b3d34b6a03fe
Victor Lourme <victor.lourme@icloud.com>
2021-03-03 18:02:17 +0100

    chore: delete dropdown
    * Unused, this was during initial testing period (tailwind instead of vuetify)

--- components/dropdown/DropdownLink.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <a
-    class="block px-4 py-2 text-sm text-gray-700 rounded-md cursor-pointer hover:bg-gray-100 hover:text-gray-900"
-    role="menuitem"
-  >
-    <slot></slot>
-  </a>
-</template>
