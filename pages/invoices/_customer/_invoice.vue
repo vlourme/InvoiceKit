7c52edaff5ecdb2bf616c62c2cbe509d873558c8
Victor Lourme <victor.lourme@icloud.com>
2021-03-05 14:58:59 +0100

    refactor(pages): commonize invoice table

--- pages/invoices/_customer/_invoice.vue
+++ pages/invoices/_customer/_invoice.vue
@@ -73,108 +73,7 @@
       </v-form>
     </Card>
 
-    <Card no-body :width="1000" class="my-4">
-      <template #title>Facturation</template>
-
-      <template #actions>
-        <v-btn text color="primary" @click="itemDialog = true">
-          <v-icon left>mdi-plus</v-icon>
-          Nouveau prix
-        </v-btn>
-      </template>
-
-      <v-data-table
-        locale="fr-fr"
-        :headers="fieldHeaders"
-        :items="invoice.data.fields"
-      >
-        <template #body="props">
-          <draggable :list="props.items" tag="tbody">
-            <tr v-for="(item, idx) in props.items" :key="idx">
-              <td>
-                <v-icon class="cursor-move">mdi-menu</v-icon>
-              </td>
-              <v-menu max-width="150" offset-y>
-                <template #activator="{ on, attrs }">
-                  <td v-bind="attrs" v-on="on">{{ item.description }}</td>
-                </template>
-                <v-list dense color="grey darken-3">
-                  <v-list-item link @click="editField(idx)">
-                    <v-list-item-icon>
-                      <v-icon>mdi-pencil</v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-content>
-                      <v-list-item-title>Modifier</v-list-item-title>
-                    </v-list-item-content>
-                  </v-list-item>
-                  <v-list-item link @click="invoice.data.fields.splice(idx, 1)">
-                    <v-list-item-icon>
-                      <v-icon>mdi-delete</v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-content>
-                      <v-list-item-title>Supprimer</v-list-item-title>
-                    </v-list-item-content>
-                  </v-list-item>
-                </v-list>
-              </v-menu>
-              <td>{{ item.quantity }}</td>
-              <td>
-                <div class="d-flex flex-column justify-center">
-                  {{ item.price }} €
-                  <span class="font-weight-bold text-caption grey--text">
-                    {{ item.tax }}% taxes
-                  </span>
-                </div>
-              </td>
-              <td>{{ invoice.getPriceAtIndex(idx) }} €</td>
-            </tr>
-          </draggable>
-        </template>
-      </v-data-table>
-    </Card>
-
-    <v-dialog v-model="itemDialog" width="500" @click:outside="closeField">
-      <v-card>
-        <v-card-title>Ajouter un objet</v-card-title>
-
-        <v-card-text>
-          <v-textarea
-            v-model="field.description"
-            label="Description"
-            rows="3"
-          ></v-textarea>
-
-          <v-row>
-            <v-col>
-              <v-text-field v-model.number="field.quantity" label="Quantité" />
-            </v-col>
-            <v-col>
-              <v-text-field
-                v-model.number="field.price"
-                label="Prix HT"
-                suffix="€"
-              />
-            </v-col>
-            <v-col>
-              <v-text-field
-                v-model.number="field.tax"
-                label="Taxes"
-                suffix="%"
-                type="number"
-              />
-            </v-col>
-          </v-row>
-        </v-card-text>
-
-        <v-divider></v-divider>
-
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="error" text @click="closeField">Annuler</v-btn>
-          <v-btn color="success" text @click="addField">Ajouter</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+    <invoice-table :invoice-state.sync="invoice" />
 
     <v-dialog v-model="promotionDialog" width="500">
       <v-card>
@@ -233,19 +132,15 @@
 </template>
 
 <script lang="ts">
-import { defaultField, FieldHeaders, InvoiceIndex, Type } from '@/types/invoice'
+import { InvoiceIndex, Type } from '@/types/invoice'
 import { cloneDeep } from 'lodash/lang'
 import Vue from 'vue'
-import draggable from 'vuedraggable'
 import { mapState } from 'vuex'
 import InvoiceImpl from '~/implementations/InvoiceImpl'
 import { NotificationType } from '~/types/notification'
 
 export default Vue.extend({
   name: 'UpdateInvoice',
-  components: {
-    draggable,
-  },
   layout: 'dashboard',
   async asyncData({ store, route }) {
     const { customer, invoice } = route.params
@@ -254,18 +149,14 @@ export default Vue.extend({
     await store.dispatch('payload/fetchInvoice', invoice)
   },
   data: () => ({
-    field: defaultField(),
     invoice: new InvoiceImpl(),
     types: [
       { text: 'Facture', value: Type.Invoice },
       { text: 'Devis', value: Type.Estimation },
     ],
-    fieldHeaders: FieldHeaders,
-    itemDialog: false,
     promotionDialog: false,
     depositDialog: false,
     dateMenu: false,
-    update: -1,
     valid: false,
   }),
   head: {
@@ -282,29 +173,6 @@ export default Vue.extend({
     this.invoice.data = cloneDeep(this.invoiceState)
   },
   methods: {
-    addField(): void {
-      if (this.update === -1) {
-        this.invoice.data.fields.push(this.field)
-      } else {
-        this.invoice.data.fields[this.update] = this.field
-      }
-
-      this.update = -1
-      this.field = defaultField()
-      this.itemDialog = false
-    },
-
-    editField(idx: number): void {
-      this.field = this.invoice.data.fields[idx]
-      this.update = idx
-      this.itemDialog = true
-    },
-
-    closeField() {
-      this.field = defaultField()
-      this.itemDialog = false
-    },
-
     async updateInvoice() {
       if (!this.customer.$key || !this.valid) {
         this.$notify('Impossible de sauvegarder', NotificationType.WARNING)
--- pages/invoices/create.vue
+++ pages/invoices/create.vue
@@ -70,108 +70,7 @@
       </v-form>
     </Card>
 
-    <Card no-body :width="1000" class="my-4">
-      <template #title>Facturation</template>
-
-      <template #actions>
-        <v-btn text color="primary" @click="itemDialog = true">
-          <v-icon left>mdi-plus</v-icon>
-          Nouveau prix
-        </v-btn>
-      </template>
-
-      <v-data-table
-        locale="fr-fr"
-        :headers="fieldHeaders"
-        :items="invoice.data.fields"
-      >
-        <template #body="props">
-          <draggable :list="props.items" tag="tbody">
-            <tr v-for="(item, idx) in props.items" :key="idx">
-              <td>
-                <v-icon class="cursor-move">mdi-menu</v-icon>
-              </td>
-              <v-menu max-width="150" offset-y>
-                <template #activator="{ on, attrs }">
-                  <td v-bind="attrs" v-on="on">{{ item.description }}</td>
-                </template>
-                <v-list dense color="grey darken-3">
-                  <v-list-item link @click="editField(idx)">
-                    <v-list-item-icon>
-                      <v-icon>mdi-pencil</v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-content>
-                      <v-list-item-title>Modifier</v-list-item-title>
-                    </v-list-item-content>
-                  </v-list-item>
-                  <v-list-item link @click="invoice.data.fields.splice(idx, 1)">
-                    <v-list-item-icon>
-                      <v-icon>mdi-delete</v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-content>
-                      <v-list-item-title>Supprimer</v-list-item-title>
-                    </v-list-item-content>
-                  </v-list-item>
-                </v-list>
-              </v-menu>
-              <td>{{ item.quantity }}</td>
-              <td>
-                <div class="d-flex flex-column justify-center">
-                  {{ item.price }} €
-                  <span class="font-weight-bold text-caption grey--text">
-                    {{ item.tax }}% taxes
-                  </span>
-                </div>
-              </td>
-              <td>{{ invoice.getPriceAtIndex(idx) }} €</td>
-            </tr>
-          </draggable>
-        </template>
-      </v-data-table>
-    </Card>
-
-    <v-dialog v-model="itemDialog" width="500">
-      <v-card>
-        <v-card-title>Ajouter un objet</v-card-title>
-
-        <v-card-text>
-          <v-textarea
-            v-model="field.description"
-            label="Description"
-            rows="3"
-          ></v-textarea>
-
-          <v-row>
-            <v-col>
-              <v-text-field v-model.number="field.quantity" label="Quantité" />
-            </v-col>
-            <v-col>
-              <v-text-field
-                v-model.number="field.price"
-                label="Prix HT"
-                suffix="€"
-              />
-            </v-col>
-            <v-col>
-              <v-text-field
-                v-model.number="field.tax"
-                label="Taxes"
-                suffix="%"
-                type="number"
-              />
-            </v-col>
-          </v-row>
-        </v-card-text>
-
-        <v-divider></v-divider>
-
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="error" text @click="itemDialog = false">Annuler</v-btn>
-          <v-btn color="success" text @click="addField">Ajouter</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+    <invoice-table :invoice-state.sync="invoice" />
 
     <v-dialog v-model="promotionDialog" width="500">
       <v-card>
@@ -233,22 +132,12 @@
 <script lang="ts">
 import Vue from 'vue'
 import { mapState } from 'vuex'
-import {
-  defaultField,
-  Field,
-  FieldHeaders,
-  InvoiceIndex,
-  Type,
-} from '@/types/invoice'
-import draggable from 'vuedraggable'
+import { InvoiceIndex, Type } from '@/types/invoice'
 import InvoiceImpl from '~/implementations/InvoiceImpl'
 import { NotificationType } from '~/types/notification'
 
 export default Vue.extend({
   name: 'CreateInvoice',
-  components: {
-    draggable,
-  },
   layout: 'dashboard',
   async asyncData({ store, route }) {
     // Type fix
@@ -258,18 +147,14 @@ export default Vue.extend({
     await store.dispatch('payload/fetchAddress', query.address)
   },
   data: () => ({
-    field: defaultField(),
     invoice: new InvoiceImpl(),
     types: [
       { text: 'Facture', value: Type.Invoice },
       { text: 'Devis', value: Type.Estimation },
     ],
-    fieldHeaders: FieldHeaders,
-    itemDialog: false,
     promotionDialog: false,
     depositDialog: false,
     dateMenu: false,
-    update: -1,
     valid: false,
   }),
   head: {
@@ -283,24 +168,6 @@ export default Vue.extend({
     this.invoice.data.address = this.address.$key
   },
   methods: {
-    addField(): void {
-      if (this.update === -1) {
-        this.invoice.data.fields.push(this.field)
-      } else {
-        this.invoice.data.fields[this.update] = this.field
-      }
-
-      this.update = -1
-      this.field = defaultField() as Field
-      this.itemDialog = false
-    },
-
-    editField(idx: number): void {
-      this.field = this.invoice.data.fields[idx]
-      this.update = idx
-      this.itemDialog = true
-    },
-
     async createInvoice() {
       if (!this.customer.$key || !this.valid) {
         this.$notify('Impossible de sauvegarder', NotificationType.WARNING)
