845ef54526790f5930c3fda6843b2e343b233bb7
Victor Lourme <victor.lourme@icloud.com>
2021-03-09 10:58:02 +0100

    refactor(components): commonize invoice editor

---
+++ components/invoice/editor.vue
@@ -0,0 +1,117 @@
+<template>
+  <Card margin :width="1000">
+    <template #title>
+      <template v-if="invoice.data.$key">
+        {{ invoice.data.type == 'QUOTE' ? 'Devis' : 'Facture ' }}
+        #{{ invoice.data.id }}
+      </template>
+      <v-template v-else>Créer une facture</v-template>
+    </template>
+
+    <template #actions>
+      <v-btn text color="success" type="submit">
+        <v-icon left>mdi-check</v-icon>
+        Créer
+      </v-btn>
+    </template>
+
+    <v-row>
+      <v-col>
+        <v-text-field
+          v-model="invoice.data.id"
+          label="Identifiant"
+          prepend-icon="mdi-pound"
+          placeholder="41-FR/2021"
+          :rules="[(v) => !!v || 'L\'identifiant est obligatoire']"
+        />
+
+        <v-select
+          v-model="invoice.data.type"
+          :items="types"
+          prepend-icon="mdi-file"
+          label="Type de document"
+        ></v-select>
+      </v-col>
+      <v-col>
+        <v-select
+          v-model="invoice.data.status"
+          :items="statuses"
+          prepend-icon="mdi-chart-line-variant"
+          label="Statut du document"
+        />
+
+        <v-dialog
+          ref="dialog"
+          v-model="dateMenu"
+          :return-value.sync="invoice.data.date"
+          persistent
+          width="290px"
+          @click:outside="dateMenu = false"
+        >
+          <template #activator="{ on, attrs }">
+            <v-text-field
+              v-model="invoice.data.date"
+              label="Date"
+              prepend-icon="mdi-calendar"
+              readonly
+              v-bind="attrs"
+              v-on="on"
+            ></v-text-field>
+          </template>
+          <v-date-picker v-model="invoice.data.date" locale="fr-fr" scrollable>
+            <v-spacer></v-spacer>
+            <v-btn text color="error" @click="dateMenu = false">
+              Annuler
+            </v-btn>
+            <v-btn
+              text
+              color="primary"
+              @click="$refs.dialog.save(invoice.data.date)"
+            >
+              Confirmer
+            </v-btn>
+          </v-date-picker>
+        </v-dialog>
+      </v-col>
+    </v-row>
+  </Card>
+</template>
+
+<script lang="ts">
+import Vue, { PropOptions } from 'vue'
+import InvoiceImpl from '~/implementations/InvoiceImpl'
+import { InvoiceStatus, InvoiceType } from '~/types/invoice'
+
+export default Vue.extend({
+  name: 'InvoiceStatus',
+  props: {
+    invoiceState: {
+      type: InvoiceImpl,
+      required: true,
+    } as PropOptions<InvoiceImpl>,
+  },
+  data: () => ({
+    types: [
+      { text: 'Facture', value: InvoiceType.Invoice },
+      { text: 'Devis', value: InvoiceType.Estimation },
+    ],
+    statuses: [
+      { text: 'Aucun', value: InvoiceStatus.None },
+      { text: 'Impayé', value: InvoiceStatus.Unpaid },
+      { text: 'En attente', value: InvoiceStatus.Pending },
+      { text: 'Payé', value: InvoiceStatus.Paid },
+    ],
+    dateMenu: false,
+  }),
+  computed: {
+    invoice: {
+      get(): InvoiceImpl {
+        return this.invoiceState
+      },
+      set(val: InvoiceImpl) {
+        this.$emit('update:invoice', val)
+      },
+    },
+  },
+})
+</script>
--- pages/invoices/create.vue
+++ pages/invoices/create.vue
@@ -1,99 +1,26 @@
 <template>
   <Header>
-    <Card margin :width="1000">
-      <template #title> Créer une facture </template>
+    <v-form v-model="valid" @submit.prevent="createInvoice">
+      <invoice-editor :invoice-state.sync="invoice" />
 
-      <template #actions>
-        <v-btn text color="success" @click="createInvoice">
-          <v-icon left>mdi-check</v-icon>
-          Créer
-        </v-btn>
-      </template>
+      <invoice-table :invoice-state.sync="invoice" />
 
-      <v-form v-model="valid">
-        <v-row>
-          <v-col>
-            <v-text-field
-              v-model="invoice.data.id"
-              label="Identifiant"
-              prepend-icon="mdi-pound"
-              placeholder="41-FR/2021"
-              :rules="[(v) => !!v || 'L\'identifiant est obligatoire']"
-            />
+      <invoice-dialog-deposit
+        :invoice-state.sync="invoice"
+        :dialog.sync="depositDialog"
+      />
+      <invoice-dialog-promotion
+        :invoice-state.sync="invoice"
+        :dialog.sync="promotionDialog"
+      />
 
-            <v-select
-              v-model="invoice.data.type"
-              :items="types"
-              prepend-icon="mdi-file"
-              label="Type de document"
-            ></v-select>
-          </v-col>
-          <v-col>
-            <v-select
-              v-model="invoice.data.status"
-              :items="statuses"
-              prepend-icon="mdi-chart-line-variant"
-              label="Statut du document"
-            />
-
-            <v-dialog
-              ref="dialog"
-              v-model="dateMenu"
-              :return-value.sync="invoice.data.date"
-              persistent
-              width="290px"
-              @click:outside="dateMenu = false"
-            >
-              <template #activator="{ on, attrs }">
-                <v-text-field
-                  v-model="invoice.data.date"
-                  label="Date"
-                  prepend-icon="mdi-calendar"
-                  readonly
-                  v-bind="attrs"
-                  v-on="on"
-                ></v-text-field>
-              </template>
-              <v-date-picker
-                v-model="invoice.data.date"
-                locale="fr-fr"
-                scrollable
-              >
-                <v-spacer></v-spacer>
-                <v-btn text color="error" @click="dateMenu = false">
-                  Annuler
-                </v-btn>
-                <v-btn
-                  text
-                  color="primary"
-                  @click="$refs.dialog.save(invoice.data.date)"
-                >
-                  Confirmer
-                </v-btn>
-              </v-date-picker>
-            </v-dialog>
-          </v-col>
-        </v-row>
-      </v-form>
-    </Card>
-
-    <invoice-table :invoice-state.sync="invoice" />
-
-    <invoice-dialog-deposit
-      :invoice-state.sync="invoice"
-      :dialog.sync="depositDialog"
-    />
-    <invoice-dialog-promotion
-      :invoice-state.sync="invoice"
-      :dialog.sync="promotionDialog"
-    />
-
-    <invoice-sidebar
-      :customer="customer"
-      :promotion-dialog.sync="promotionDialog"
-      :deposit-dialog.sync="depositDialog"
-      :invoice="invoice"
-    />
+      <invoice-sidebar
+        :customer="customer"
+        :promotion-dialog.sync="promotionDialog"
+        :deposit-dialog.sync="depositDialog"
+        :invoice="invoice"
+      />
+    </v-form>
   </Header>
 </template>
 
@@ -116,19 +43,8 @@ export default Vue.extend({
   },
   data: () => ({
     invoice: new InvoiceImpl(),
-    types: [
-      { text: 'Facture', value: InvoiceType.Invoice },
-      { text: 'Devis', value: InvoiceType.Estimation },
-    ],
-    statuses: [
-      { text: 'Aucun', value: InvoiceStatus.None },
-      { text: 'Impayé', value: InvoiceStatus.Unpaid },
-      { text: 'En attente', value: InvoiceStatus.Pending },
-      { text: 'Payé', value: InvoiceStatus.Paid },
-    ],
     promotionDialog: false,
     depositDialog: false,
-    dateMenu: false,
     valid: false,
   }),
   head: {
