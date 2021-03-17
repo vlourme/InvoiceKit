cbcaea2b41f11666714da95ab41fe2559f12cd5f
Victor Lourme <victor.lourme@icloud.com>
2021-03-17 16:49:10 +0100

    feat(pages): add stats on dashboard

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -2,6 +2,41 @@
   <div>
     <Header>
       <template #title>Tableau de bord</template>
+
+      <v-row justify="center">
+        <v-col>
+          <v-card>
+            <v-card-text>
+              <p class="display-1 text--primary">
+                {{ team.counter.invoices }}
+              </p>
+              <p>Factures</p>
+            </v-card-text>
+          </v-card>
+        </v-col>
+
+        <v-col>
+          <v-card>
+            <v-card-text>
+              <p class="display-1 text--primary">
+                {{ team.counter.customers }}
+              </p>
+              <p>Clients</p>
+            </v-card-text>
+          </v-card>
+        </v-col>
+
+        <v-col>
+          <v-card>
+            <v-card-text>
+              <p class="display-1 text--primary">
+                {{ team.counter.addresses }}
+              </p>
+              <p>Adresses</p>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
     </Header>
   </div>
 </template>
@@ -18,6 +53,7 @@ export default {
   }),
   computed: {
     ...mapState('auth', ['user']),
+    ...mapState('team', ['team']),
   },
 }
 </script>
