5507b691e7a5b71991fdbfdc77ff8a94c83d0f25
Victor Lourme <victor.lourme@icloud.com>
2021-03-21 19:17:23 +0100

    refactor(functions): folder renaming * Functions in DistributedCounter does not always concern the counter and sometimes do something else.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,9 +4,9 @@ import * as admin from 'firebase-admin'
 admin.initializeApp()
 
 // Export functions
-export * from './distributedCounter/customers/onCreate'
-export * from './distributedCounter/customers/onUpdate'
-export * from './distributedCounter/customers/onDelete'
-export * from './distributedCounter/customers/invoices/onCreate'
-export * from './distributedCounter/customers/invoices/onUpdate'
-export * from './distributedCounter/customers/invoices/onDelete'
+export * from './customers/onCreate'
+export * from './customers/onUpdate'
+export * from './customers/onDelete'
+export * from './customers/invoices/onCreate'
+export * from './customers/invoices/onUpdate'
+export * from './customers/invoices/onDelete'
