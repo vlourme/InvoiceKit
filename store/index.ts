4a716ebf5bdca3f8bc43d00e5fd50f81df4d8896
Victor Lourme <victor.lourme@icloud.com>
2021-04-06 14:33:07 +0200

    refactor(store): dropped payload module

--- store/index.ts
+++ store/index.ts
@@ -1,11 +1,9 @@
 import { AuthModuleState } from './auth'
 import { NotificationModuleState } from './notification'
-import { PayloadModuleState } from './payload'
 import { TeamModuleState } from './team'
 
 export default interface RootState {
   auth: AuthModuleState
   team: TeamModuleState
   notification: NotificationModuleState
-  payload: PayloadModuleState
 }
