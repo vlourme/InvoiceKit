eacb64000f144065bbc8e07d1d7bc5fed155d9c6
Victor Lourme <victor.lourme@icloud.com>
2021-04-06 16:00:55 +0200

    refactor(layouts): redesigned dashboard sidebar * Support links are not yet available, just for visual

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -1,76 +1,74 @@
 <template>
   <div class="flex w-full h-screen bg-white divide-x">
-    <nav class="flex flex-col h-screen max-w-xs w-full shadow-md bg-gray-50">
+    <nav class="flex flex-col h-screen max-w-xs w-full bg-gray-100">
       <div class="flex-1">
-        <p class="text-center text-gray-700 font-bold text-2xl px-4 py-6">
-          InvoiceKit <span class="font-light text-gray-500">Suite</span>
-        </p>
-
-        <aside class="py-2">
-          <template v-for="link in links">
-            <nuxt-link
-              :key="link.name"
-              :to="link.route"
-              class="px-6 py-4 inline-flex items-center hover:bg-gray-200 transition-all w-full"
-            >
-              <i
-                :class="{
-                  'text-indigo-500': $route.path.startsWith(link.route),
-                  [link.icon]: true,
-                }"
-                class="bx text-xl text-gray-700"
-              ></i>
-
-              <div
-                :class="{
-                  'text-indigo-500': $route.path.startsWith(link.route),
-                }"
-                class="ml-6 font-medium text-gray-600"
-              >
-                {{ link.name }}
-              </div>
-            </nuxt-link>
-          </template>
-        </aside>
-      </div>
-
-      <div v-if="user" class="relative inline-block text-left">
-        <div
-          class="flex items-center justify-center w-full hover:bg-gray-200 py-6 cursor-pointer"
-          @click="open = !open"
-        >
-          <img :src="user.image" class="rounded-full h-12 w-12" />
-
-          <div class="px-4">
-            <p class="leading-tight font-semibold text-lg">
-              {{ user.name }}
+        <div class="p-6 flex justify-between items-center">
+          <div class="text-3xl">
+            <p class="font-medium text-gray-500">
+              Invoice<span class="font-semibold text-gray-600">Kit</span>
             </p>
-            <p class="text-sm">{{ user.email }}</p>
           </div>
+          <Dropdown v-if="user">
+            <img
+              class="rounded-full h-11 w-11 cursor-pointer"
+              :src="user.image"
+              @click="open = !open"
+            />
+
+            <template #content>
+              <div class="py-1" role="none">
+                <nuxt-link
+                  to="/account/settings"
+                  class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                  role="menuitem"
+                  >Paramètres du compte</nuxt-link
+                >
+                <a
+                  class="block cursor-pointer px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                  role="menuitem"
+                  @click="logout"
+                  >Se deconnecter</a
+                >
+              </div>
+            </template>
+          </Dropdown>
         </div>
 
-        <div
-          :class="{ block: open, hidden: !open }"
-          class="origin-top-right absolute bottom-0 right-0 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
-          role="menu"
-          aria-orientation="vertical"
-          aria-labelledby="options-menu"
-        >
-          <div class="py-1" role="none">
-            <nuxt-link
-              to="/account/settings"
-              class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
-              role="menuitem"
-              >Paramètres du compte</nuxt-link
-            >
-            <a
-              class="block cursor-pointer px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
-              role="menuitem"
-              @click="logout"
-              >Se deconnecter</a
-            >
+        <aside class="px-6">
+          <div v-for="link in links" :key="link.name" class="my-2">
+            <p class="font-semibold text-xs uppercase text-gray-400">
+              {{ link.name }}
+            </p>
+            <div class="py-1">
+              <nuxt-link
+                v-for="item in link.routes"
+                :key="item.route"
+                :to="item.route"
+                :class="{
+                  'bg-white rounded-md shadow': $route.path.startsWith(
+                    item.route
+                  ),
+                }"
+                class="my-2 flex items-center w-full py-2 px-3 text-gray-500 cursor-pointer group hover:bg-white hover:bg-opacity-50 rounded-md transition-colors"
+              >
+                <i
+                  :class="{
+                    [item.icon]: true,
+                    'text-indigo-600': $route.path.startsWith(item.route),
+                  }"
+                  class="bx text-xl mr-4"
+                ></i>
+
+                <p
+                  :class="{ 'font-bold': $route.path.startsWith(item.route) }"
+                  class="font-medium"
+                >
+                  {{ item.name }}
+                </p>
+              </nuxt-link>
+            </div>
           </div>
-        </div>
+        </aside>
       </div>
     </nav>
 
@@ -87,7 +85,8 @@
 import {
   computed,
   defineComponent,
-  ref,
+  reactive,
+  toRefs,
   useContext,
   useRouter,
   useStore,
@@ -104,27 +103,47 @@ export default defineComponent({
     const router = useRouter()
 
     // Data
-    const open = ref(false)
-    const links = ref([
-      {
-        route: '/dashboard',
-        name: 'Tableau de bord',
-        icon: 'bxs-home',
-        teamRequired: false,
-      },
-      {
-        route: '/customers',
-        name: 'Clients',
-        icon: 'bxs-user',
-        teamRequired: true,
-      },
-      {
-        route: '/invoices',
-        name: 'Factures',
-        icon: 'bxs-cabinet',
-        teamRequired: true,
-      },
-    ])
+    const data = reactive({
+      open: false,
+      links: [
+        {
+          name: 'General',
+          routes: [
+            {
+              route: '/dashboard',
+              name: 'Tableau de bord',
+              icon: 'bxs-home',
+            },
+            {
+              route: '/customers',
+              name: 'Clients',
+              icon: 'bxs-user',
+            },
+            {
+              route: '/invoices',
+              name: 'Factures',
+              icon: 'bxs-cabinet',
+            },
+          ],
+        },
+        {
+          name: 'Support',
+          routes: [
+            {
+              route: '/support',
+              name: 'Aide & Support',
+              icon: 'bxs-message',
+            },
+            {
+              route: '/issues',
+              absolute: true,
+              name: 'Signaler un bug',
+              icon: 'bxs-bug',
+            },
+          ],
+        },
+      ],
+    })
 
     // Computed
     const user = computed(() => store.state.auth.user)
@@ -136,7 +155,7 @@ export default defineComponent({
       await router.push({ path: '/' })
     }
 
-    return { open, links, user, logout }
+    return { ...toRefs(data), user, logout }
   },
 })
 </script>
