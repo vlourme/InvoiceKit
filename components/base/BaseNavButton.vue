347148339b08e10742692ec3cc77134a16541509
Victor Lourme <victor.lourme@icloud.com>
2021-04-06 14:29:19 +0200

    feat(components/base): add disabled variant

--- components/base/BaseNavButton.vue
+++ components/base/BaseNavButton.vue
@@ -2,7 +2,7 @@
   <button
     v-bind="$attrs"
     type="button"
-    class="bg-gray-200 bg-opacity-50 h-14 px-4 inline-flex font-medium items-center hover:bg-opacity-100 focus:outline-none"
+    class="bg-gray-200 bg-opacity-50 disabled:opacity-60 disabled:pointer-events-none h-14 px-4 inline-flex font-medium items-center hover:bg-opacity-100 focus:outline-none"
     v-on="$listeners"
   >
     <slot></slot>
