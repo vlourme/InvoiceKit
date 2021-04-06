2eb25b41a7007e4b36326d1af03446e645866238
Victor Lourme <victor.lourme@icloud.com>
2021-04-06 15:00:58 +0200

    refactor(components/base): add color transition on focus

--- components/base/BaseNavInput.vue
+++ components/base/BaseNavInput.vue
@@ -2,7 +2,7 @@
   <input
     v-bind="$attrs"
     type="text"
-    class="bg-gray-200 bg-opacity-50 hover:bg-opacity-100 focus:bg-opacity-100 focus:outline-none px-4 h-full"
+    class="bg-gray-200 bg-opacity-50 hover:bg-opacity-100 focus:bg-opacity-100 focus:outline-none transition-colors px-4 h-full"
     @input="$emit('input', $event.target.value)"
   />
 </template>
