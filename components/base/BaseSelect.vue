4492c3bd0eb2d99db334641427abe6c08aff4f29
Victor Lourme <victor.lourme@icloud.com>
2021-04-09 10:17:44 +0200

    feat(components/base): select can cast as boolean

--- components/base/BaseSelect.vue
+++ components/base/BaseSelect.vue
@@ -3,7 +3,12 @@
     <select
       v-bind="$attrs"
       class="w-full inline-block disabled:opacity-60 appearance-none mt-1 px-3 py-2 bg-gray-50 focus:outline-none focus:border-indigo-500 transition-colors rounded-md border-2 border-gray-200"
-      @input="$emit('input', $event.target.value)"
+      @input="
+        $emit(
+          'input',
+          bool ? $event.target.value == 'true' : $event.target.value
+        )
+      "
     >
       <template v-for="item in items">
         <option :key="item.value" :value="item.value">
@@ -29,6 +34,10 @@ export default defineComponent({
       type: Array,
       required: true,
     } as PropOptions<SelectItem[]>,
+    bool: {
+      type: Boolean,
+      required: false,
+    } as PropOptions<boolean>,
   },
 })
 </script>
