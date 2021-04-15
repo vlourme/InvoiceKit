35e79a90839eb192c6ffeedc04e944d006ba4c67
Victor Lourme <victor.lourme@icloud.com>
2021-04-15 16:17:52 +0200

    feat(components): add algolia logo on search input

--- components/base/BaseNavInput.vue
+++ components/base/BaseNavInput.vue
@@ -1,8 +1,15 @@
 <template>
-  <input
-    v-bind="$attrs"
-    type="text"
-    class="px-4 py-2 mr-2 rounded-md bg-gray-200 text-gray-800 border-2 border-transparent inline-flex font-medium items-center hover:bg-gray-300 focus:outline-none focus:border-gray-200"
-    @input="$emit('input', $event.target.value)"
-  />
+  <div class="relative mr-2">
+    <input
+      v-bind="$attrs"
+      type="text"
+      class="px-4 py-2 rounded-md bg-gray-200 text-gray-800 border-2 border-transparent inline-flex font-medium items-center hover:bg-gray-300 focus:outline-none focus:border-gray-200 w-full"
+      @input="$emit('input', $event.target.value)"
+    />
+    <div
+      class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3"
+    >
+      <i class="bx bxl-algolia text-2xl text-blue-600"></i>
+    </div>
+  </div>
 </template>
